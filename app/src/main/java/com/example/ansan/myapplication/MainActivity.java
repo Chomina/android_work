f039dd83eacc327e57ea1160d8cf117f3cfb46dd
chomina <alsk8368@naver.com>
2016-11-02 11:06:01 +0900

    메인엑티비디 변경

--- app/src/main/java/com/example/ansan/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ansan/myapplication/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //메인엑티비티 변경
     }
+
 }
